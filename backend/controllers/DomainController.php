ef8e875cae3aa9af6062461850d7e74d14e8cfeb
ArtemMakolov <makolov55@gmail.com>
2018-08-03 16:19:56 +0300

    new update project

--- backend/controllers/DomainController.php
+++ backend/controllers/DomainController.php
@@ -95,19 +95,6 @@ class DomainController extends Controller
         ]);
     }
 
-    /**
-     * Deletes an existing ProjectDomain model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionDelete($id)
-    {
-        $this->findModel($id)->delete();
-
-        return $this->redirect(['index']);
-    }
 
     /**
      * Finds the ProjectDomain model based on its primary key value.
