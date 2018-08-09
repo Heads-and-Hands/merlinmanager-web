51b62ca4db0b2bec8c83abe7308a899ab6a99e29
ArtemMakolov <makolov55@gmail.com>
2018-08-09 17:18:25 +0300

    update

--- backend/controllers/DomainController.php
+++ backend/controllers/DomainController.php
@@ -2,12 +2,17 @@
 
 namespace backend\controllers;
 
+use Alchemy\Zippy\Zippy;
+use backend\models\DomainForm;
+use function PHPSTORM_META\elementType;
 use Yii;
 use backend\models\ProjectDomain;
 use yii\data\ActiveDataProvider;
+use yii\helpers\FileHelper;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\web\UploadedFile;
 
 /**
  * DomainController implements the CRUD actions for ProjectDomain model.
@@ -57,23 +62,6 @@ class DomainController extends Controller
         ]);
     }
 
-    /**
-     * Creates a new ProjectDomain model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
-    {
-        $model = new ProjectDomain();
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        }
-
-        return $this->render('create', [
-            'model' => $model,
-        ]);
-    }
 
     /**
      * Updates an existing ProjectDomain model.
@@ -85,16 +73,48 @@ class DomainController extends Controller
     public function actionUpdate($id)
     {
         $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
+        $domainForm = new DomainForm();
+        $domainForm->id = $model->id;
+        $domainForm->domain = $model->domain;
+        if (!$domainForm->load(Yii::$app->request->post())) {
+            return $this->render('update', [
+                'model' => $domainForm,
+            ]);
         }
 
-        return $this->render('update', [
-            'model' => $model,
-        ]);
+        $domainForm->file = UploadedFile::getInstance($domainForm, 'file');
+        if (!$domainForm->validate()) {
+            return $this->render('update', [
+                'model' => $domainForm,
+            ]);
+        }
+
+        $model->domain = $domainForm->domain;
+        if (!$model->save()) {
+            return $this->render('update', [
+                'model' => $domainForm,
+            ]);
+        }
+
+        $projectFolder = $this->updateArchive($domainForm);
+        FileHelper::unlink(Yii::getAlias('@webPath') . '/' . $domainForm->file->name);
+        if (!$projectFolder){
+            return $this->render('update', [
+                'model' => $domainForm,
+            ]);
+        }
+        return $this->redirect(['view', 'id' => $domainForm->id]);
     }
 
+    public function updateArchive($domainForm)
+    {
+        $domainForm->file->saveAs(Yii::getAlias('@webPath') . '/' . $domainForm->file->name);
+        $zippy = Zippy::load();
+        $zipAdapter = $zippy->getAdapterFor('zip');
+        $archive = $zipAdapter->open(Yii::getAlias('@webPath') . '/' . $domainForm->file->name);
+        $archive->extract(Yii::getAlias('@webPath'));
+        return true;
+    }
 
     /**
      * Finds the ProjectDomain model based on its primary key value.
